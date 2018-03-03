085bd218c8150cf3d321efdcd76b168a26be90af
SwethaTestGit <37003282+SwethaTestGit@users.noreply.github.com>
2018-03-02 21:14:02 -0500

    Added Test 2 in the comments

--- WcfService1/IService1.cs
+++ WcfService1/IService1.cs
@@ -12,7 +12,7 @@ namespace WcfService1
     [ServiceContract]
     public interface IService1
     {
-        /* added a comment    - spaces  Test1   */
+        /* added a comment    - spaces  Test1 Test 2  */
         [OperationContract]
         string GetData(int value);
 
