3a08b97caeca307dbaa518acebfd7023c0ef9847
SwethaTestGit <37003282+SwethaTestGit@users.noreply.github.com>
2018-03-02 21:18:33 -0500

    Added test 3

--- WcfService1/IService1.cs
+++ WcfService1/IService1.cs
@@ -12,7 +12,7 @@ namespace WcfService1
     [ServiceContract]
     public interface IService1
     {
-        /* added a comment    - spaces  Test1 Test 2  */
+        /* added a comment    - spaces  Test1 Test 2 Test 3  */
         /* added comment on Git */
         [OperationContract]
         string GetData(int value);
